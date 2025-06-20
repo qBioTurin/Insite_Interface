f1a51fbba555f067dc36531000254ef65fe47c31
Gepiro <gepiro0916@gmail.com>
2025-06-20 12:39:11 +0200

    Addde ':' in relative frequencies

--- interface/src/components/relative-frequencies.tsx
+++ interface/src/components/relative-frequencies.tsx
@@ -1,4 +1,4 @@
-import { Badge, Center, NumberInput, ScrollAreaAutosize } from "@mantine/core";
+import { Badge, Center, NumberInput, Text, ScrollAreaAutosize } from "@mantine/core";
 import { colors } from "./colors";
 import { Event } from "./interfaces";
 
@@ -11,17 +11,26 @@ export default function RelativeFrequencies({ events, updateEventFrequency }: { 
 			<ScrollAreaAutosize mt={10}>
 				<div style={{ display: 'flex', gap: 20, width: 'max-content' }}>
 					{events.map((event, index) => (
-						<div key={index} style={{ padding: 'lg', height: 100 }}>
-							<Center mb={5}>
-								<Badge color={colors[event.type]} size="xl" >{event.name}</Badge>
-							</Center>
-							<Center>
-								<NumberInput defaultValue={event.frequency} w={100} onChange={(value) => updateEventFrequency(event, Number(value))} hideControls />
-							</Center>
-						</div>
+						<>
+							<div key={index} style={{ padding: 'lg', height: 100 }}>
+								<Center mb={5}>
+									<Badge color={colors[event.type]} size="xl" >{event.name}</Badge>
+								</Center>
+								<Center>
+									<NumberInput defaultValue={event.frequency} w={100} onChange={(value) => updateEventFrequency(event, Number(value))} hideControls />
+								</Center>
+							</div>
+							{events.length > index + 1?
+								<Center>
+									<Text>
+										:
+									</Text>
+								</Center > : <></>
+							}
+						</>
 					))}
 				</div>
-			</ScrollAreaAutosize>
+			</ScrollAreaAutosize >
 
 		</>
 	);
