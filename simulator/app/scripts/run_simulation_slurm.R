a6406af48c10c170d8d4fa6c80deeffe9a2a4937
DanielaVolpatto <volpattodaniela@gmail.com>
2025-08-13 11:01:30 +0200

    Aggiustati input per slurm

--- simulator/app/scripts/run_simulation_slurm.R
+++ simulator/app/scripts/run_simulation_slurm.R
@@ -8,6 +8,12 @@ source("scripts/Simulazioni_fenotipo_classi.R")
 
 option_list<-list(
   make_option(
+    c("--Nexp"),
+    type="integer",
+    default = 1,
+    help = "simulation number"
+  ),
+  make_option(
     c("--seed"),
     type="integer",
     default = as.integer(Sys.time()),
@@ -20,24 +26,36 @@ option_list<-list(
     help = "RData file created by import_par_slurm"
   ),
   make_option(
-    c("--dir"),
+    c("--input_dir"),
     type="character",
     default = "raw",
-    help = "directory for the files"
+    help = "directory for the files in input and the seed"
+  ),
+  make_option(
+    c("--output_dir"),
+    type="character",
+    default = "raw",
+    help = "directory for the simulation output"
   )
 )
 
 opt_parser<-OptionParser(option_list = option_list)
 opt<-parse_args(opt_parser)
 
-path<-opt$dir
+path_in<-opt$input_dir
+path_out<-opt$output_dir
+
 params_RData<-opt$params_RData
 
-load(paste(path,"/Parameters.RData",sep=""))
+load(paste(path_in,"/Parameters.RData",sep=""))
 
-simulazione(Nexp=1,
+Nexp<-opt$Nexp
+
+write(opt$seed,file=paste(path_in,"/seed.txt",sep=""))
+
+simulazione(Nexp=Nexp,
             seed=opt$seed,
-            path = path,
+            path = path_out,
             starting_gen = starting_gen,
             starting_fun_eff=starting_fun_eff,
             tmax=tmax,
